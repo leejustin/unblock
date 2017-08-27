cb5dc724308f2b06369c7689784178ba7966976e
Justin <leejustin@users.noreply.github.com>
2017-08-26 21:54:14 -0700

    Add functions to convert to JSON for our DB

--- js/helper.js
+++ js/helper.js
@@ -14,3 +14,49 @@ function toastPersonCreated(id) {
 function toastClearAll() {
     toastr.remove();
 }
+
+
+/* Convert to JSON for DB */
+function createPersonDto(alias, x, y, z) {
+    return {
+        'alias': alias,
+        'position': {
+            'x': x,
+            'y': y,
+            'z': z
+        },
+        'state': 'default',
+        'type': 'sphere'
+    }
+}
+
+function createBlockingDto(formations, width, length, userId) {
+    return {
+        'formations': formations,
+        'dimension': {
+            'width': width,
+            'length': length,
+        },
+        'createdBy': userId,
+        'createdAt': null
+    }
+}
+
+function convertFormationsToDto(formations) {
+    var convertedFormations = [];
+
+    for (i = 0; i < formations.length; i++) {
+        var convertedPersons = [];
+        for (j = 0; j < formations[i].length; j++) {
+            convertedPersons.push(
+                createPersonDto(formations[i][j]['alias'],
+                    formations[i][j]['position']['x'] / SCALE_FACTOR,
+                    formations[i][j]['position']['y'] / SCALE_FACTOR,
+                    formations[i][j]['position']['z'] / SCALE_FACTOR));
+        }
+        convertedFormations.push(convertedPersons);
+    }
+
+    return convertedFormations;
+}
+
